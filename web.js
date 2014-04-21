87edc71c50c9f98a2c12276a775ae1370b92c821
rbarzyczak <roman.barzyczak@brainly.com>
2014-04-21 12:15:18 +0200

    add websocket test

--- web.js
+++ web.js
@@ -13,3 +13,19 @@ var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+var wss = new WebSocketServer({server: server});
+console.log('websocket server created');
+wss.on('connection', function(ws) {
+  var id = setInterval(function() {
+    ws.send(JSON.stringify(new Date()), function() {  });
+  }, 1000);
+
+  console.log('websocket connection open');
+
+  ws.on('close', function() {
+    console.log('websocket connection close');
+    clearInterval(id);
+  });
+});

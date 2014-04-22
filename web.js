428022fd0e6fa114be33e18613d3aca761066ccf
rbarzyczak <roman.barzyczak@brainly.com>
2014-04-22 21:37:41 +0200

    add only websocket example

--- web.js
+++ web.js
@@ -1,37 +1,27 @@
-// app.js
+var WebSocketServer = require('ws').Server
+  , http = require('http')
+  , express = require('express')
+  , app = express()
+  , port = process.env.PORT || 5000;
 
-var http = require('http'); 
+app.use(express.static(__dirname + '/'));
 
 var server = http.createServer(app);
 server.listen(port);
 
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
-
-app.use(logfmt.requestLogger());
-
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
-
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
-
+console.log('http server listening on %d', port);
 
 var wss = new WebSocketServer({server: server});
 console.log('websocket server created');
 wss.on('connection', function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  });
-  }, 1000);
+    var id = setInterval(function() {
+        ws.send(JSON.stringify(new Date()), function() {  });
+    }, 1000);
 
-  console.log('websocket connection open');
+    console.log('websocket connection open');
 
-  ws.on('close', function() {
-    console.log('websocket connection close');
-    clearInterval(id);
-  });
+    ws.on('close', function() {
+        console.log('websocket connection close');
+        clearInterval(id);
+    });
 });
